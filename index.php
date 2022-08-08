c45c7fefb411b4e054916a5ec9a54886648e6444
totisoto5 <totii.sotoo@gmail.com>
2022-08-08 17:08:51 -0300

    link de insta

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php require ('./layout/menu.php') ?>
 
     <div class="Redes" id="Redes">
-        <img src="img/insta.png" alt="Instagram">
-        <img src="img/face.png" alt="Instagram">
-        <img src="img/wp.png" alt="Instagram">
+        <a href="https://www.instagram.com/lachapa.dx/" target="_blank"><img  src="img/insta.png" alt="Instagram"></a>
+        <a href="https://www.google.com/" target="_blank"><img src="img/face.png" alt="Facebook"></a>
+        <a href="https://www.google.com/" target="_blank"><img src="img/wp.png" alt="WhatsApp"></a>
     </div> 
 
 
