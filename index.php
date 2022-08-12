7c79b11050242aaf7d6a40861eb2465095602ddd
totisoto5 <totii.sotoo@gmail.com>
2022-08-12 03:10:06 -0300

    Quité el form del index

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php require ('./layout/menu.php') ?>
 
-    <div class="Redes" id="Redes">
+    <span class="Redes" id="Redes">
         <a href="https://www.instagram.com/lachapa.dx/" target="_blank"><img  src="img/insta.png" alt="Instagram"></a>
         <a href="https://www.google.com/" target="_blank"><img src="img/face.png" alt="Facebook"></a>
         <a href="https://www.google.com/" target="_blank"><img src="img/wp.png" alt="WhatsApp"></a>
-    </div> 
+    </span> 
+
 
 
 <?php require ('./layout/footer.php') ?>
